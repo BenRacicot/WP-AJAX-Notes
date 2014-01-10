b2169cf23600a6b2c352f855cdf6c134a6a6e9c4
Ben Racicot <benjaminracicot@gmail.com>
2014-01-10 15:23:41 -0500

    init commit

---
+++ AJAX.php
@@ -0,0 +1,114 @@
+
+<?php// header.php 
+
+Bad practice: 
+function custom_head(){ echo '<script type="text/javascript">var ajaxurl = \''.admin_url('admin-ajax.php').'\';</script>'; }
+
+Good practice:
+function my_theme_js_init() { 
+
+	wp_enqueue_script('my_theme_init', get_stylesheet_directory_uri() . '/library/js/init.js', array('jquery'));
+	wp_localize_script( 'my_theme_init', 'My_Obj', array( 
+		'ajaxurl' => admin_url( 'admin-ajax.php' )
+		) 
+	); // create nonce here
+
+}
+add_action('wp_enqueue_scripts', 'my_theme_js_init');
+
+
+
+?>
+
+
+<script type="text/javascript">
+// Ben's ajax JS functions
+$(window).load(function(){
+
+	var myOBJ = []; // must create empty object var
+  var count = 1; 
+
+  // Next button
+	$('#ajaxnext').on('click', function(){
+	  count++;
+		myOBJ = {
+			action: 'pages',
+			data: "page_no=" + count,
+			count:count
+		};
+ 	  loadArticles(myOBJ);
+ 	  console.log(myOBJ);
+  }); 
+
+	// Prev button
+  // $('#ajaxprev').on('click', function(){
+  // 	if(count != 1) {count--};
+		// myOBJ = {
+		// 	action: 'pages',
+		// 	data: "page_no=" + count,
+		// 	count:count
+		// };
+ 	//   loadArticles(myOBJ);
+  // }); 
+
+  // Query
+  function loadArticles(pageNumber){
+    $.ajax({
+        url: ajaxurl, // defined in header.php
+        type:'POST',
+        data: myOBJ, 
+        success: function(html){
+	     	  $('.firstrow').empty(); // emtpy the node
+	     	  $('.firstrow').append(html); // append the returned data to the node
+	     	  console.log(myOBJ.count);
+        }
+    });
+		return false;
+  }
+	// Query on initial load
+	myOBJ = {
+		action: 'pages',
+		data: "page_no=" + count,
+		count:count
+	};
+	loadArticles(myOBJ);
+
+});
+// End Ben's JS ajax functions
+</script>
+
+
+
+
+<?php // PHP code for AJAX 
+
+add_action('wp_ajax_pages', '_custom_paginate');           // for logged in user
+add_action('wp_ajax_nopriv_pages', '_custom_paginate');    // if user not logged in
+
+function _custom_paginate(){ 
+
+	$paged = $_POST['count'];
+	//$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+	$args = array(
+	  'posts_per_page'  => 12,
+	  'category'				=> 3,
+	  'paged' 					=> $paged, 
+	  'post_status'     => 'publish'
+	); 
+
+  $query = new WP_Query($args);
+
+	  if ( $query->have_posts() ) :
+	  while ($query->have_posts()) : $query->the_post(); ?>
+	  <div class="content">
+	      <?php the_content();?>
+	  </div>
+
+
+		<?php endwhile; endif; exit;
+}// End AJAX
+
+
+
+
